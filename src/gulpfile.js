e68d9112b2b06ded16d6cf937ff8b46dd5d866fc
ZhuMingYang <332595512@qq.com>
2019-10-29 17:44:05 +0800

    add tsc task

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,31 +1,42 @@
-const gulp = require('gulp')
-const less = require('gulp-less')
-const webpack = require('webpack-stream')
-const config = require('./webpack.config.js')
+const gulp = require("gulp");
+const less = require("gulp-less");
+const webpack = require("webpack-stream");
+const config = require("./webpack.config.js");
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
 
 // 源文件夹 --> parser --> 目标文件夹
 
 // 转译js
-gulp.task('webpack', () => {
+gulp.task("webpack", () => {
   gulp
-    .src('./js/**/*.js')
+    .src("./js/**/*.js")
     .pipe(webpack(config))
-    .pipe(gulp.dest('../www/js'))
-})
+    .pipe(gulp.dest("../www/js"));
+});
+
+// 转译ts
+gulp.task("tsc", () => {
+  gulp
+    .src(tsProject.config.include)
+    .pipe(tsProject())
+    .pipe(gulp.dest(tsProject.config.compilerOptions.outDir));
+});
 
 // 编译less
-gulp.task('less', () => {
+gulp.task("less", () => {
   gulp
-    .src('./less/*.less')
+    .src("./less/*.less")
     .pipe(less())
-    .pipe(gulp.dest('../www/css'))
-})
+    .pipe(gulp.dest("../www/css"));
+});
 
 // gulp
-gulp.task('default', ['webpack', 'less'])
+gulp.task("default", ["webpack", "tsc", "less"]);
 
 // gulp watch
-gulp.task('watch', () => {
-  gulp.watch('./less/*.less', ['less'])
-  gulp.watch('./js/**/*.js', ['webpack'])
-})
+gulp.task("watch", () => {
+  gulp.watch("./less/*.less", ["less"]);
+  gulp.watch("./ts/*.ts", ["tsc"]);
+  gulp.watch("./js/**/*.js", ["webpack"]);
+});
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   entry: {
-    index: './js/index'
+    index: "./js/index"
   },
   output: {
-    filename: '[name].js'
+    filename: "[name].js"
   },
-  devtool: 'source-map', // 跟踪源文件哪个地方出错
+  devtool: "source-map", // 跟踪源文件哪个地方出错
   resolve: {
-    extensions: ['.js'] // 自动解析某些扩展,这是用户在导入时可以忽略扩展名的原因(当用户import一个文件夹，如果有这个语句，这回自动解析js文件)
+    extensions: [".js"] // 自动解析某些扩展,这是用户在导入时可以忽略扩展名的原因(当用户import一个文件夹，如果有这个语句，这回自动解析js文件)
   },
   module: {
     rules: [
@@ -15,13 +15,13 @@ module.exports = {
         test: /\.js$/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env']
+              presets: ["@babel/preset-env"]
             }
           }
         ]
       }
     ]
   }
-}
+};
