40782098cd8036bbce5f3908966fc630d52a956d
ZhuMingYang <332595512@qq.com>
2019-10-30 19:19:03 +0800

    update tsconfig

--- src/ts/hello.ts
+++ src/ts/hello.ts
@@ -1,7 +1,8 @@
-function getLength(something: string | number): number {
-    if ((<string>something).length) {
-        return (<string>something).length;
-    } else {
-        return something.toString().length;
-    }
+class GenericNumber<T> { // 泛型参数也可以设置默认类型
+  zeroValue: T;
+  add: (x: T, y: T) => T;
 }
+
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) { return x + y; };
